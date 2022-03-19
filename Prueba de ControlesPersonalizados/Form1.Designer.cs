18a577680d2a3d68751184a374062688d8069b4c
José Antonio Balbuena <JoseABalbuena2@hotmail.com>
2022-03-18 23:34:56 -0600

    Agregar archivos de proyecto.

---
+++ Prueba de ControlesPersonalizados/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Prueba_de_ControlesPersonalizados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbDarkTheme = new Botón_personalizado2.RJControls.RJToggleButton();
+            this.DarkTheme = new System.Windows.Forms.Label();
+            this.WhiteTheme = new System.Windows.Forms.Label();
+            this.rjButton1 = new CustomControls.RJControls.RJButton();
+            this.rjButton2 = new CustomControls.RJControls.RJButton();
+            this.rjButton3 = new CustomControls.RJControls.RJButton();
+            this.rjButton4 = new CustomControls.RJControls.RJButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.picture1 = new System.Windows.Forms.PictureBox();
+            this.picture3 = new System.Windows.Forms.PictureBox();
+            this.picture2 = new System.Windows.Forms.PictureBox();
+            this.picture4 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbDarkTheme
+            // 
+            this.tbDarkTheme.AutoSize = true;
+            this.tbDarkTheme.Location = new System.Drawing.Point(23, 30);
+            this.tbDarkTheme.MinimumSize = new System.Drawing.Size(45, 22);
+            this.tbDarkTheme.Name = "tbDarkTheme";
+            this.tbDarkTheme.OffBackColor = System.Drawing.Color.Gray;
+            this.tbDarkTheme.OffToggleColor = System.Drawing.Color.Gainsboro;
+            this.tbDarkTheme.OnBackColor = System.Drawing.Color.MediumSlateBlue;
+            this.tbDarkTheme.OnToggleColor = System.Drawing.Color.WhiteSmoke;
+            this.tbDarkTheme.Size = new System.Drawing.Size(45, 22);
+            this.tbDarkTheme.TabIndex = 0;
+            this.tbDarkTheme.UseVisualStyleBackColor = true;
+            this.tbDarkTheme.CheckedChanged += new System.EventHandler(this.tbDarkTheme_CheckedChanged);
+            // 
+            // DarkTheme
+            // 
+            this.DarkTheme.AutoSize = true;
+            this.DarkTheme.Location = new System.Drawing.Point(1, 11);
+            this.DarkTheme.Name = "DarkTheme";
+            this.DarkTheme.Size = new System.Drawing.Size(88, 16);
+            this.DarkTheme.TabIndex = 1;
+            this.DarkTheme.Text = "Modo Oscuro";
+            this.DarkTheme.Visible = false;
+            // 
+            // WhiteTheme
+            // 
+            this.WhiteTheme.AutoSize = true;
+            this.WhiteTheme.Location = new System.Drawing.Point(1, 11);
+            this.WhiteTheme.Name = "WhiteTheme";
+            this.WhiteTheme.Size = new System.Drawing.Size(77, 16);
+            this.WhiteTheme.TabIndex = 2;
+            this.WhiteTheme.Text = "Modo Claro";
+            // 
+            // rjButton1
+            // 
+            this.rjButton1.BackColor = System.Drawing.Color.Blue;
+            this.rjButton1.BackgroundColor = System.Drawing.Color.Blue;
+            this.rjButton1.BackgroundColor1 = System.Drawing.Color.Blue;
+            this.rjButton1.BorderColor = System.Drawing.Color.Black;
+            this.rjButton1.BorderRadius = 9;
+            this.rjButton1.BorderSize = 0;
+            this.rjButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rjButton1.FlatAppearance.BorderSize = 0;
+            this.rjButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton1.ForeColor = System.Drawing.Color.White;
+            this.rjButton1.Location = new System.Drawing.Point(108, 11);
+            this.rjButton1.Name = "rjButton1";
+            this.rjButton1.Size = new System.Drawing.Size(150, 40);
+            this.rjButton1.TabIndex = 3;
+            this.rjButton1.Text = "Imagen 1";
+            this.rjButton1.TextColor = System.Drawing.Color.White;
+            this.rjButton1.UseVisualStyleBackColor = false;
+            this.rjButton1.Click += new System.EventHandler(this.rjButton1_Click);
+            // 
+            // rjButton2
+            // 
+            this.rjButton2.BackColor = System.Drawing.Color.Blue;
+            this.rjButton2.BackgroundColor = System.Drawing.Color.Blue;
+            this.rjButton2.BackgroundColor1 = System.Drawing.Color.Blue;
+            this.rjButton2.BorderColor = System.Drawing.Color.Black;
+            this.rjButton2.BorderRadius = 9;
+            this.rjButton2.BorderSize = 0;
+            this.rjButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rjButton2.FlatAppearance.BorderSize = 0;
+            this.rjButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton2.ForeColor = System.Drawing.Color.White;
+            this.rjButton2.Location = new System.Drawing.Point(284, 12);
+            this.rjButton2.Name = "rjButton2";
+            this.rjButton2.Size = new System.Drawing.Size(150, 40);
+            this.rjButton2.TabIndex = 4;
+            this.rjButton2.Text = "Imagen 2";
+            this.rjButton2.TextColor = System.Drawing.Color.White;
+            this.rjButton2.UseVisualStyleBackColor = false;
+            this.rjButton2.Click += new System.EventHandler(this.rjButton2_Click);
+            // 
+            // rjButton3
+            // 
+            this.rjButton3.BackColor = System.Drawing.Color.Blue;
+            this.rjButton3.BackgroundColor = System.Drawing.Color.Blue;
+            this.rjButton3.BackgroundColor1 = System.Drawing.Color.Blue;
+            this.rjButton3.BorderColor = System.Drawing.Color.Black;
+            this.rjButton3.BorderRadius = 9;
+            this.rjButton3.BorderSize = 0;
+            this.rjButton3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rjButton3.FlatAppearance.BorderSize = 0;
+            this.rjButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton3.ForeColor = System.Drawing.Color.White;
+            this.rjButton3.Location = new System.Drawing.Point(449, 11);
+            this.rjButton3.Name = "rjButton3";
+            this.rjButton3.Size = new System.Drawing.Size(150, 40);
+            this.rjButton3.TabIndex = 5;
+            this.rjButton3.Text = "Imagen 3";
+            this.rjButton3.TextColor = System.Drawing.Color.White;
+            this.rjButton3.UseVisualStyleBackColor = false;
+            this.rjButton3.Click += new System.EventHandler(this.rjButton3_Click);
+            // 
+            // rjButton4
+            // 
+            this.rjButton4.BackColor = System.Drawing.Color.Blue;
+            this.rjButton4.BackgroundColor = System.Drawing.Color.Blue;
+            this.rjButton4.BackgroundColor1 = System.Drawing.Color.Blue;
+            this.rjButton4.BorderColor = System.Drawing.Color.Black;
+            this.rjButton4.BorderRadius = 9;
+            this.rjButton4.BorderSize = 0;
+            this.rjButton4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rjButton4.FlatAppearance.BorderSize = 0;
+            this.rjButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton4.ForeColor = System.Drawing.Color.White;
+            this.rjButton4.Location = new System.Drawing.Point(621, 11);
+            this.rjButton4.Name = "rjButton4";
+            this.rjButton4.Size = new System.Drawing.Size(150, 40);
+            this.rjButton4.TabIndex = 6;
+            this.rjButton4.Text = "Imagen 4";
+            this.rjButton4.TextColor = System.Drawing.Color.White;
+            this.rjButton4.UseVisualStyleBackColor = false;
+            this.rjButton4.Click += new System.EventHandler(this.rjButton4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.picture4);
+            this.panel1.Controls.Add(this.picture2);
+            this.panel1.Controls.Add(this.picture3);
+            this.panel1.Controls.Add(this.picture1);
+            this.panel1.Location = new System.Drawing.Point(4, 70);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(792, 375);
+            this.panel1.TabIndex = 7;
+            // 
+            // picture1
+            // 
+            this.picture1.Image = ((System.Drawing.Image)(resources.GetObject("picture1.Image")));
+            this.picture1.Location = new System.Drawing.Point(19, 19);
+            this.picture1.Name = "picture1";
+            this.picture1.Size = new System.Drawing.Size(286, 152);
+            this.picture1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture1.TabIndex = 0;
+            this.picture1.TabStop = false;
+            this.picture1.Visible = false;
+            // 
+            // picture3
+            // 
+            this.picture3.Image = ((System.Drawing.Image)(resources.GetObject("picture3.Image")));
+            this.picture3.Location = new System.Drawing.Point(19, 205);
+            this.picture3.Name = "picture3";
+            this.picture3.Size = new System.Drawing.Size(286, 152);
+            this.picture3.TabIndex = 1;
+            this.picture3.TabStop = false;
+            this.picture3.Visible = false;
+            // 
+            // picture2
+            // 
+            this.picture2.Image = ((System.Drawing.Image)(resources.GetObject("picture2.Image")));
+            this.picture2.Location = new System.Drawing.Point(445, 19);
+            this.picture2.Name = "picture2";
+            this.picture2.Size = new System.Drawing.Size(286, 152);
+            this.picture2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture2.TabIndex = 2;
+            this.picture2.TabStop = false;
+            this.picture2.Visible = false;
+            this.picture2.Click += new System.EventHandler(this.picture2_Click);
+            // 
+            // picture4
+            // 
+            this.picture4.Image = ((System.Drawing.Image)(resources.GetObject("picture4.Image")));
+            this.picture4.Location = new System.Drawing.Point(445, 205);
+            this.picture4.Name = "picture4";
+            this.picture4.Size = new System.Drawing.Size(286, 152);
+            this.picture4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picture4.TabIndex = 3;
+            this.picture4.TabStop = false;
+            this.picture4.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.rjButton4);
+            this.Controls.Add(this.rjButton3);
+            this.Controls.Add(this.rjButton2);
+            this.Controls.Add(this.rjButton1);
+            this.Controls.Add(this.WhiteTheme);
+            this.Controls.Add(this.DarkTheme);
+            this.Controls.Add(this.tbDarkTheme);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Botón_personalizado2.RJControls.RJToggleButton tbDarkTheme;
+        private System.Windows.Forms.Label DarkTheme;
+        private System.Windows.Forms.Label WhiteTheme;
+        private CustomControls.RJControls.RJButton rjButton1;
+        private CustomControls.RJControls.RJButton rjButton2;
+        private CustomControls.RJControls.RJButton rjButton3;
+        private CustomControls.RJControls.RJButton rjButton4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picture4;
+        private System.Windows.Forms.PictureBox picture2;
+        private System.Windows.Forms.PictureBox picture3;
+        private System.Windows.Forms.PictureBox picture1;
+    }
+}
+
---
+++ Prueba de ControlesPersonalizados/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba_de_ControlesPersonalizados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbDarkTheme_CheckedChanged(object sender, EventArgs e)
+        {
+            if (tbDarkTheme.Checked)
+            {
+                this.BackColor = Color.DimGray;
+                DarkTheme.ForeColor = Color.White;
+                DarkTheme.Visible= true;
+                WhiteTheme.Visible= false;
+            }
+            else
+            {
+                this.BackColor = Color.WhiteSmoke;
+                DarkTheme.Visible = false;
+                WhiteTheme.ForeColor = Color.Black;
+                WhiteTheme.Visible = true;
+            }
+        }
+
+
+        private void rjButton1_Click(object sender, EventArgs e)
+        {
+            picture1.Visible = true;
+        }
+
+        private void rjButton2_Click(object sender, EventArgs e)
+        {
+            picture2.Visible = true;
+        }
+
+        private void rjButton3_Click(object sender, EventArgs e)
+        {
+            picture3.Visible = true;
+        }
+
+        private void rjButton4_Click(object sender, EventArgs e)
+        {
+            picture4.Visible = true;
+        }
+
+        private void picture2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
